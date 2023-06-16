287e36024264c3a263b70ff1e98064435146571b
unknown <lpramithamj@gmail.com>
2023-06-16 11:01:21 +0530

    update map list

--- map_library_addlist.cpp
+++ map_library_addlist.cpp
@@ -4,10 +4,24 @@
 #include<list>
 
 using namespace std;
+
 int main() {   
     map<string,list<string>> pokedex;
 
     list<string> pikachuAttacts {"thunder shock","tail whip","quick attact"};
+    list<string> charmanderAttacts {"flame thrower","scary face"};
+    list<string> chikoritaAttacts {"razor leaf","poison powder"};
+
+    pokedex.insert(pair<string,list<string>>("Pikkuchu",pikachuAttacts));
+    pokedex.insert(pair<string,list<string>>("charmander",charmanderAttacts));
+    pokedex.insert(pair<string,list<string>>("chikorita",chikoritaAttacts));
+
+    for(auto pair :pokedex){
+        cout<<pair.first<<" - ";
+
+        for(auto attact : pair.second)
+            cout<<attact<<", ";
     
-    
+        cout<<endl;
+    }
 }
